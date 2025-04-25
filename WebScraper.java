7699c1e04eb828a3cee2a3707e3feb26da7ad24b
DilshanVimukthi <vdilshan2021@gmail.com>
2025-04-25 16:03:26 +0530

    Update WebScraper.java

--- WebScraper.java
+++ WebScraper.java
@@ -1,9 +1,38 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.IOException;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WebScraper {
     public static void main(String[] args) {
-        // TODO: Scrape a URL and print its title
+       String url = "https://bbc.com";
+        try {
+    		Document doc =Jsoup.connect(url).get();
+    		
+    		String title = doc.title();
+    		System.out.println("Title " + title);
+    		System.out.println("Headings");
+    		
+    		for(int i =1; i<=6; i++) {
+    			Elements headings = doc.select("h" + i);
+    			for(Element heading : headings) {
+    				System.out.println("h" + i + ": "+ heading.text());
+    			}
+    		}
+    		
+    		System.out.println("Links");
+    		
+    		Elements links = doc.select("a[href]");
+    		for(Element link : links) {
+    			String linkHref = link.attr("abs:href");
+    			String linkText = link.text();
+    			System.out.println(linkText + "->" + linkHref);
+    			}
+    	
+    		}catch(IOException e) {
+    			System.out.println("Error: " + e.getMessage());
+    		
+    	}
     }
 }
